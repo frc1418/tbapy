b613a4e892e9f6a05a0a5e359fd65a68d7b82a1b
Ian W <ian.uu12@gmail.com>
2017-04-27 00:05:56 -0700

    Dynamically generate model classes

--- example.py
+++ example.py
@@ -11,7 +11,9 @@ match = tba.match(year=2017,
                   type='sf',
                   number=2,
                   round=1)
+robots = tba.team_robots(4131)
 
 print('Team 254 is from %s in %s, %s.' % (team.city, team.state_prov, team.country))
 print('Team 1418 is/was in the %s district in the most recent year of competition.' % districts[-1].display_name)
 print('The second qual match at the 2017 CHS District Championship was predicted to start at Unix Time %s.' % match.predicted_time)
+print('Team 4131\'s robots: ' + ', '.join('%s (%d)' % (robot.robot_name, robot.year) for robot in robots))
