d04b0f6ff6ccd0b02031dd06ee7c472736e9706b
ErikBoesen <me@erikboesen.com>
2017-11-22 12:19:42 -0500

    Increment version to 1.3.0

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from setuptools import setup
 
 setup(name='tbapy',
-      version='1.2.3',
-      description='Python library to get data from The Blue Alliance API v3.',
+      version='1.3.0',
+      description='Library for interacting with The Blue Alliance API.',
       url='https://github.com/frc1418/tbapy',
       author='FRC Team 1418, Erik Boesen',
       author_email='robotics1418@gmail.com',
