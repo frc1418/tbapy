f7184d54ff062609dcf15db4f56832263ec052a1
ErikBoesen <me@erikboesen.com>
2019-03-18 20:47:40 -0400

    Bump to 1.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tbapy',
-      version='1.3.0',
+      version='1.3.1',
       description='Library for interacting with The Blue Alliance API.',
       url='https://github.com/frc1418/tbapy',
       author='FRC Team 1418, Erik Boesen',
