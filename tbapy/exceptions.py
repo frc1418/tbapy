1589da90f6efe8fadee8d6054424a4f5b9726e1a
AndrewLester <alester220@gmail.com>
2019-11-24 22:53:11 -0500

    Move exceptions to respective files

--- tbapy/exceptions.py
+++ tbapy/exceptions.py
@@ -2,3 +2,16 @@ class TBAErrorList(Exception):
     def __init__(self, errors):
         self.errors = errors
         super().__init__('\n'.join([f'Error type ({error[0]}): {error[1]}' for error in errors]))
+
+
+class LastModifiedDate:
+    def __init__(self, date_string):
+        self.date_string = date_string
+
+    def __bool__(self):
+        return False
+
+
+class NotModifiedException(Exception):
+    def __init__(self, last_modified):
+        self.last_modified = LastModifiedDate(last_modified)
--- tbapy/main.py
+++ tbapy/main.py
@@ -7,20 +7,6 @@ from .exceptions import *
 from cachecontrol import CacheControl
 
 
-
-class LastModifiedDate:
-    def __init__(self, date_string):
-        self.date_string = date_string
-
-    def __bool__(self):
-        return False
-
-
-class NotModifiedException(Exception):
-    def __init__(self, last_modified):
-        self.last_modified = LastModifiedDate(last_modified)
-
-
 class TBA:
     """
     Main library class.
